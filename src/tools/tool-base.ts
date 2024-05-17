53d8a4326eb7b1d67f2a195e357b92bc6b1e5eb0
Oguz Tas <mimoguz@outlook.com>
2024-05-17 19:21:58 +0300

    new base

--- src/tools/tool-base.ts
+++ src/tools/tool-base.ts
@@ -1,4 +1,4 @@
-import { Point } from "../common";
+import { Point, point } from "../common";
 import { Bitmap } from "../schema";
 import { Tool } from "./tool";
 import { ToolOptions, ToolResult } from "./tool-result";
@@ -48,5 +48,95 @@ export abstract class ToolBase implements Tool {
     protected reset() {
         this.clear()
         this.bmp = undefined
+    }
+}
+
+
+/**
+ * Base class for tools.
+ * Override handle... methods to add functionality 
+ * */
+export abstract class ToolBase2 implements Tool {
+    handleInit(): void { }
+    handleStart(): void { }
+    handleProgress(): void { }
+    handleEnd(): ToolResult | undefined { return undefined }
+    handleCancel(): void { }
+    handleOptionsChanged(): void { }
+    handleContextChanged(): void { }
+    handleCursor(currentPt: Point): void { this.ctx?.fillRect(currentPt.x, currentPt.y, 1, 1) }
+
+    protected ctx: CanvasRenderingContext2D | undefined
+
+    protected opt: ToolOptions = {
+        color: "black",
+        brushSize: 1
+    }
+
+    protected isDrawing: boolean = false
+
+    protected pt: Point = point.outside()
+
+    protected bmp: Bitmap | undefined = undefined
+
+    get context(): CanvasRenderingContext2D | undefined {
+        return this.ctx
+    }
+
+    set context(value: CanvasRenderingContext2D | undefined) {
+        this.ctx = value
+        this.handleContextChanged()
+    }
+
+    get options(): ToolOptions {
+        return this.opt
+    }
+
+    set options(value: ToolOptions) {
+        this.opt = value
+        this.handleOptionsChanged()
+    }
+
+    start(pt: Point, bmp: Bitmap) {
+        this.bmp = bmp
+        this.pt = pt
+        this.isDrawing = true
+        this.handleStart()
+    }
+
+    moveTo(pt: Point): void {
+        if (point.equals(pt, this.pt)) return
+        this.pt = pt
+        if (this.isDrawing) {
+            this.handleProgress()
+        } else {
+            this.handleCursor(pt)
+        }
+    }
+
+    end(pt: Point): ToolResult | undefined {
+        const result = this.handleEnd()
+        this.reset()
+        this.handleCursor(pt)
+        return result
+    }
+
+
+    cancel(): void {
+        this.handleCancel()
+        this.reset()
+    }
+
+    protected clear() {
+        if (this.context) {
+            this.context.clearRect(0, 0, this.context.canvas.clientWidth, this.context.canvas.clientHeight)
+        }
+    }
+
+    protected reset() {
+        this.clear()
+        this.bmp = undefined
+        this.pt = point.outside()
+        this.isDrawing = false
     }
 }
